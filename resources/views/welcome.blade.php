40909e3afb3d74a5f0aa98e22b18a5c0c0b85104
Kdichon <herve@kdichon.fr>
2023-06-19 12:20:34 -0400

    Setting TailwindCSS - Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>The Shop</title>
+    @vite('resources/css/app.css')
     @livewireStyles
 </head>
 
 <body>
-    ...
+
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 
     @livewireScripts
 </body>
